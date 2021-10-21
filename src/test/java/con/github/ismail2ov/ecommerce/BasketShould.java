e80a6afe11d376d84bbe18cc8956f91c895adce9
Ismail Ahmedov <iahmedov@paradigmadigital.com>
2021-10-21 13:55:57 +0200

    Refactor

--- src/test/java/con/github/ismail2ov/ecommerce/BasketShould.java
+++ src/test/java/con/github/ismail2ov/ecommerce/BasketShould.java
@@ -1,5 +1,6 @@
 package con.github.ismail2ov.ecommerce;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.UUID;
@@ -7,22 +8,25 @@ import java.util.UUID;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class BasketShould {
+    private Basket basket;
+    private Customer customer;
+    private BasketRepository basketRepository;
+
+    @BeforeEach
+    void setUp() {
+        UUID customerId = UUID.randomUUID();
+        customer = new Customer(customerId, "Pepe");
+        basketRepository = new FakeBasketRepository();
+        basket = new Basket(customer, basketRepository);
+    }
+
     @Test
     void has_no_items_when_is_initialized() {
-        UUID customerId = UUID.randomUUID();
-        Customer customer = new Customer(customerId, "Pepe");
-        BasketRepository basketRepository = new FakeBasketRepository();
-        Basket basket = new Basket(customer, basketRepository);
-
         assertThat(basket.getItems()).hasSize(0);
     }
 
     @Test
     void restore_last_client_basket() {
-        BasketRepository basketRepository = new FakeBasketRepository();
-        UUID customerId = UUID.randomUUID();
-        Customer customer = new Customer(customerId, "Pepe");
-        Basket basket = new Basket(customer, basketRepository);
         Item item = new Item("iPod", 99);
         basket.addItem(item);
 
